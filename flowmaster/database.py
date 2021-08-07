0e862d2efecfd11ee382d3d7355b53a6b82b8bad
pmaksimov <vur21@ya.ru>
2021-08-07 11:20:11 +0300

    autostart database

--- flowmaster/database.py
+++ flowmaster/database.py
@@ -13,7 +13,7 @@ db = SqliteQueueDatabase(
         ("foreign_keys", 1),
     ),
     use_gevent=False,  # Use the standard library "threading" module.
-    autostart=False,  # The worker thread now must be started manually.
+    autostart=True,  # The worker thread now must be started manually.
     queue_max_size=64,  # Max. # of pending writes that can accumulate.
     results_timeout=3.0,  # Max. time to wait for query to be executed.
 )
